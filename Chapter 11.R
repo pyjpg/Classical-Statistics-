5de12895da10290acc4f1742fff702c2ef7b0ec0
pyjpg <titas.r@outlook.com>
2026-02-17 13:13:49 +0000

    Exercise 11.b started

--- Chapter 11.R
+++ Chapter 11.R
@@ -1,6 +1,9 @@
 # Clear all variables
 rm(list = ls())
 
+# Install package MASS for leverages
+install.packages("MASS")
+library(MASS)
 # Example 11.1 ~ stirring revs per minute | impurity percentages
 # create stirring variable going from 20 - 42 in increments of 2
 stirring.rpm <- seq(20,42,2)
@@ -34,6 +37,17 @@ paint.fitted.values
 paint.residuals <- residuals(paint.linear.model)
 paint.residuals
 
+# Calculate the leverages using lm.influence()
+paint.leverages <- lm.influence(paint.linear.model)$hat
+paint.leverages
+
+# Get the standard resdiauls using stdres() of the linear model
+standard_res <- stdres(paint.linear.model)
+standard_res
+
+# Create a data frame to store the leverages againist the standard residuals
+paint.diagnostics <- data.frame("leverages"=paint.leverages, "Standard residuals"=standard_res)
+paint.diagnostics
 
 
 # Exercise 11.1
@@ -53,3 +67,13 @@ kitty.anova
 
 # There tends to be a somewhat linear relationship between initial weight and weight gain, with on average being 1.064 * initial weight larger however this is not very strong
 
+
+
+# Exercise 11.b) Is there anything suspect
+
+# My intuition tells me that this should be a comparsion againist leverages vs standard residuals and we should be able to outline
+# What are our benchmarks for leverages and Standard residuals
+
+# Leverages = h_hat = 2/n where n in our case is the number of kitty cats we got which is 15, so 2/15 x 3 = 2/5(0.4) is our benchmark for leverages
+# Standard residuals benchmark is 95% of values within +- 2 so we need to find out the 95% CI for this
+kitty.data
