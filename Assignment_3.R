18b43af631030b1f1ea7acf13cd28a4246e3941d
pyjpg <titas.r@outlook.com>
2026-03-15 14:52:06 +0000

    question 2b complete

--- Assignment_3.R
+++ Assignment_3.R
@@ -51,3 +51,19 @@ lower_bound <- difference - lsd
 
 upper_bound
 lower_bound
+
+
+# Question 2
+
+# Create a data frame subset only containing viriginca flowers petal length and petal width.
+virginica_petal_info <- subset.data.frame(data_iris, data_iris$Species=="virginica", select = c(Petal.Length,
+                                                                                                Petal.Width))
+
+# Form a linear regression model 
+virginica_petal_info.linearmodel <- lm(Petal.Length ~ Petal.Width, data = virginica_petal_info)
+
+# Use summary to inspect the values in the linear regression model
+summary(virginica_petal_info.linearmodel)
+
+
+
